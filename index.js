500019e12dedd3b22c3b983b800eeb3fa33ab0ea
Michael Jester <m.jester.93@gmail.com>
2020-09-17 22:03:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function addUpTo(array, index) {
 
 function maxOf(array) {
   if (array.length < 1) {
-    return array[0]
+    return array
   } else {
     if (array[0] > array[array.length - 1]) {
       return maxOf(array.slice(1, array.length))
