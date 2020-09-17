b9b26ffe838f8ca8fc316cb177378a3adc92c9c7
Michael Jester <m.jester.93@gmail.com>
2020-09-17 21:36:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution here!
 function printString(string) {
   console.log(string)
-  
+
   if (string.length > 1) {
     let mySubString = string.substring(1, string.length)
     printString(mySubString)
