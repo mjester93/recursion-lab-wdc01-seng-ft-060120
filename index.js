52e958bf3f79045a28716ea64556dea6e67f5dd5
Michael Jester <m.jester.93@gmail.com>
2020-09-17 21:36:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // Code your solution here!
-
+function printString(string) {
+  console.log(string)
+  
+  if (string.length > 1) {
+    let mySubString = string.substring(1, string.length)
+    printString(mySubString)
+  } else {
+    return true
+  }
+}
