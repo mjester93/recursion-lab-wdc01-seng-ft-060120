ead956922d30529545030997d8fcea53ab7b033c
Michael Jester <m.jester.93@gmail.com>
2020-09-17 21:46:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,7 @@ function reverseString(string) {
     return reverseString(string.substring(1)) + string[0];
   }
 }
+
+function isPalindrome(string) {
+  return string === reverseString(string)
+}
